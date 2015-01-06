5b62ef8d7fbf6d9938ba5e61b946b87b64b007fa
Dave Menendez <davemm@cs.rutgers.edu>
2015-01-06 01:37:04 -0500

    Clean up codegen.py

--- codegen.py
+++ codegen.py
@@ -2,19 +2,19 @@ from pretty import *
 from collections import defaultdict
 
 class CFragment(object):
-	'Common superclass for C statements and expressions.'
+  'Common superclass for C statements and expressions.'
 
   INDENT=2
 
 class CTest(CFragment):
-	def __init__(self, string):
-		self.s = string
-	
-	def format(self):
-		return text(self.s + ';')
-	
-	def formatExpr(self, prec):
-		return text(self.s)
+  def __init__(self, string):
+    self.s = string
+
+  def format(self):
+    return text(self.s + ';')
+
+  def formatExpr(self, prec):
+    return text(self.s)
 
 class CType(CFragment):
   pass
@@ -59,106 +59,106 @@ class CTypeName(CType):
 
 
 class CExpression(CFragment):
-	def pprint(self, width=80, prec=0):
-		self.formatExpr(prec).pprint(width)
+  def pprint(self, width=80, prec=0):
+    self.formatExpr(prec).pprint(width)
 
-	def dot(self, field, args=None):
-		return CFieldAccess(self, field, args)
+  def dot(self, field, args=None):
+    return CFieldAccess(self, field, args)
 
-	def arr(self, field, args=None):
-		return CFieldAccess(self, field, args, direct=False)
+  def arr(self, field, args=None):
+    return CFieldAccess(self, field, args, direct=False)
 
-	def format(self):
-		return self.formatExpr(18) + ';'
+  def format(self):
+    return self.formatExpr(18) + ';'
 
 class CVariable(CExpression):
-	def __init__(self, name):
-		self.name = name
-	
-	def formatExpr(self, prec=0):
-		return text(self.name)
+  def __init__(self, name):
+    self.name = name
+
+  def formatExpr(self, prec=0):
+    return text(self.name)
 
 class CFunctionCall(CExpression):
-	def __init__(self, fun, *args):
-		self.fun = fun
-		self.args = args
-	
-	def formatExpr(self, prec = 0):
-		if len(self.args) == 0:
-			return text(self.fun + '()')
-		
-		arglist = iter_seq(joinit((arg.formatExpr(18) for arg in self.args), ',' + line))
-		
-		return group(self.fun + '(' + arglist + ')').nest(4)
+  def __init__(self, fun, *args):
+    self.fun = fun
+    self.args = args
 
-# 		return group(self.fun + '(' + self.args[0].formatExpr(18) + 
-# 			iter_seq(',' + line + arg.formatExpr(18) for arg in self.args[1:]) + ')').nest(4)
+  def formatExpr(self, prec = 0):
+    if len(self.args) == 0:
+      return text(self.fun + '()')
+
+    arglist = iter_seq(joinit((arg.formatExpr(18) for arg in self.args), ',' + line))
+
+    return group(self.fun + '(' + arglist + ')').nest(4)
+
+#     return group(self.fun + '(' + self.args[0].formatExpr(18) + 
+#       iter_seq(',' + line + arg.formatExpr(18) for arg in self.args[1:]) + ')').nest(4)
 
 class CFieldAccess(CExpression):
-	def __init__(self, src, field, args = None, direct = True):
-		self.src = src
-		self.field = field
-		self.args = args
-		self.direct = direct
-	
-	def formatExpr(self, prec = 0):
-		op = '.' if self.direct else '->'
-		
-		s = self.src.formatExpr(0) + op + self.field
-		if self.args != None:
-			s += group(nest(2, '(' + iter_seq(joinit((arg.formatExpr(18) for arg in self.args), ',' + line))
-							+ ')'))
-		
-		return s
+  def __init__(self, src, field, args = None, direct = True):
+    self.src = src
+    self.field = field
+    self.args = args
+    self.direct = direct
+
+  def formatExpr(self, prec = 0):
+    op = '.' if self.direct else '->'
+
+    s = self.src.formatExpr(0) + op + self.field
+    if self.args != None:
+      s += group(nest(2, '(' + iter_seq(joinit((arg.formatExpr(18) for arg in self.args), ',' + line))
+              + ')'))
+
+    return s
 
 
 class CUnaryExpr(CExpression):
-	def __init__(self, op, x):
-		self.op = op
-		self.x = x
-	
-	def formatExpr(self, prec = 0):
-		return nest(2, self.op + self.x.formatExpr(3))
+  def __init__(self, op, x):
+    self.op = op
+    self.x = x
+
+  def formatExpr(self, prec = 0):
+    return nest(2, self.op + self.x.formatExpr(3))
 
 class CBinExpr(CExpression):
-	@staticmethod
-	def reduce(op, args):
-		return reduce(lambda a,b: CBinExpr(op,a,b), args)
+  @staticmethod
+  def reduce(op, args):
+    return reduce(lambda a,b: CBinExpr(op,a,b), args)
 
-	def __init__(self, op, x, y):
-		self.op = op
-		self.x = x
-		self.y = y
-	
-	_prec = {
-		'*': 5, '/': 5, '%': 5,
-		'+': 6, '-': 6,
-		'<<': 7, '>>': 7,
-		'<': 8, '<=': 8, '>': 8, '>=': 8,
-		'==': 9, '!=': 9,
-		'&': 10,
-		'^': 11,
-		'|': 12,
-		'&&': 13,
-		'||': 14,
-		'=': 15,
-		',': 17,
-	}
-	_lassoc = {'/', '%', '-', '<<', '>>', '<', '<=', '>', '>=', '==', '!='}
-	
-	def formatExpr(self, prec = 0):
-		p = self._prec[self.op]
-		rp = p
-		if self.op in self._lassoc: rp -= 1
-		
-		
-		fmt = self.x.formatExpr(p) + line + self.op + ' ' + self.y.formatExpr(rp)
-		if prec < p or (10 <= prec and prec <= 12 and p <= 6):
-			fmt = group(nest(2, '(' + fmt + ')'))
-		elif prec > p:
-			fmt = group(fmt).nest(2)
+  def __init__(self, op, x, y):
+    self.op = op
+    self.x = x
+    self.y = y
 
-		return fmt
+  _prec = {
+    '*': 5, '/': 5, '%': 5,
+    '+': 6, '-': 6,
+    '<<': 7, '>>': 7,
+    '<': 8, '<=': 8, '>': 8, '>=': 8,
+    '==': 9, '!=': 9,
+    '&': 10,
+    '^': 11,
+    '|': 12,
+    '&&': 13,
+    '||': 14,
+    '=': 15,
+    ',': 17,
+  }
+  _lassoc = {'/', '%', '-', '<<', '>>', '<', '<=', '>', '>=', '==', '!='}
+
+  def formatExpr(self, prec = 0):
+    p = self._prec[self.op]
+    rp = p
+    if self.op in self._lassoc: rp -= 1
+
+
+    fmt = self.x.formatExpr(p) + line + self.op + ' ' + self.y.formatExpr(rp)
+    if prec < p or (10 <= prec and prec <= 12 and p <= 6):
+      fmt = group(nest(2, '(' + fmt + ')'))
+    elif prec > p:
+      fmt = group(fmt).nest(2)
+
+    return fmt
 
 class CAssign(CExpression):
   def __init__(self, x, y):
@@ -177,23 +177,23 @@ class CAssign(CExpression):
     return fmt
 
 class CStatement(CFragment):
-	def pprint(self, width=80):
-		self.format().pprint(width)
+  def pprint(self, width=80):
+    self.format().pprint(width)
 
 class CIf(CStatement):
-	def __init__(self, condition, then_block, else_block=[]):
-		self.condition = condition
-		self.then_block = then_block
-		self.else_block = else_block
-	
-	def format(self):
-		f = 'if (' + group(nest(4, self.condition.formatExpr(18) + ')') + line) + '{' + \
-			nest(4, iter_seq(line + s.format() for s in self.then_block)) + line + '}'
-		
-		if self.else_block:
-			f = f + ' else {' + nest(4, iter_seq(line + s.format() for s in self.else_block)) + line + '}'
-		
-		return f
+  def __init__(self, condition, then_block, else_block=[]):
+    self.condition = condition
+    self.then_block = then_block
+    self.else_block = else_block
+
+  def format(self):
+    f = 'if (' + group(nest(4, self.condition.formatExpr(18) + ')') + line) + '{' + \
+      nest(4, iter_seq(line + s.format() for s in self.then_block)) + line + '}'
+
+    if self.else_block:
+      f = f + ' else {' + nest(4, iter_seq(line + s.format() for s in self.else_block)) + line + '}'
+
+    return f
 
 
 class CDefinition(CStatement):
@@ -233,52 +233,13 @@ class CDefinition(CStatement):
 
 
 class CReturn(CStatement):
-	def __init__(self, ret = None):
-		assert ret == None or isinstance(ret, CExpression)
-		self.ret = ret
+  def __init__(self, ret = None):
+    assert ret == None or isinstance(ret, CExpression)
+    self.ret = ret
 
-	def format(self):
-		f = text('return')
-		if self.ret != None:
-			f += nest(2, line + self.ret.formatExpr(18))
-		f += ';'
-		return group(f)
-
-test = CIf(CBinExpr('&&', CTest('foo'), CBinExpr('&&', CTest('bar'), CTest('baz'))),
-	[CTest('stmt'), CTest('stmt')])
-
-test2 = CIf(CBinExpr('&&', CTest('foo'), CBinExpr('&&', CTest('bar'), CTest('baz'))),
-	[CTest('stmt'), CTest('stmt')], [CTest('stmt'), CTest('stmt')])
-
-
-if __name__ == '__main__':
-	a,b,c,d,e = (CVariable(n) for n in ('alpha','beta','gamma','delta','epsilon'))
-
-	CIf(CBinExpr('==', a, b), [CTest('stmt')]).pprint()
-
-	x = CIf(CBinExpr('&&', 
-			CBinExpr('==', a, b),
-			CBinExpr('&&',
-				CBinExpr('==', a, b),
-				CBinExpr('&&',
-					CBinExpr('<=', CBinExpr('|', d, e), c),
-					CBinExpr('<=', c,	CBinExpr('&', d, e))))),
-		[CTest('statement')],
-		[CTest('stmt'), CTest('stmt')])
-	x.pprint(120)
-	x.pprint(80)
-	x.pprint(30)
-	
-	f = lambda x,y: CFunctionCall('WillNotOverflowSignedAdd', x, y)
-	
-	x = CBinExpr('||', a, CBinExpr('||', CBinExpr('&&', f(c,e), f(c,b)), d))
-	x.pprint(80)
-	x.pprint(40)
-#	x.pprint(20)
-#	x.pprint(10)
-	
-	y = CBinExpr('&&', a, CBinExpr('&&', CBinExpr('||', f(b,c), f(b,e)), d))
-	y.pprint(80)
-	y.pprint(40)
-#	y.pprint(20)
-#	y.pprint(10)
+  def format(self):
+    f = text('return')
+    if self.ret != None:
+      f += nest(2, line + self.ret.formatExpr(18))
+    f += ';'
+    return group(f)
