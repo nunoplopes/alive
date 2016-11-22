d4631935e23b4ed1879d90b3850f12b346f07527
Nuno Lopes <nuno.lopes@ist.utl.pt>
2016-11-22 15:56:15 +0000

    bump Copyright year

--- alive.py
+++ alive.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-# Copyright 2014-2015 The Alive authors.
+# Copyright 2014-2016 The Alive authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
